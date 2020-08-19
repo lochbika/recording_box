dcad0e56b09d365659d2eaa4e41f8add93eb8d7b
kai <kai_lochi@gmx.de>
2020-08-19 17:45:00 +0200

    working rotary encoder and buttons; basically ready to implement the menu and other routines

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # initialize
-from gpiozero import CPUTemperature
+from gpiozero import LED
 from RPLCD.i2c import CharLCD
 from RPi import GPIO
 import time
@@ -19,13 +19,17 @@ inc_new = 0
 # LCD
 lcd = CharLCD('PCF8574', 0x27, cols=16, rows=2)
 
+# LEDs
+record_ledpin = 24
+record_led = LED(record_ledpin)
 
 # Welcome message
 lcd.write_string('Hey Kai, how are you doing?')
 time.sleep(2)
 
-def display_write(message=" ", x=0, y=0):
-	lcd.clear()
+def display_write(message=" ", x=0, y=0, clear=1):
+	if clear == 1:
+		lcd.clear()
 	lcd.cursor_pos = (y, x)
 	lcd.write_string(message)
 
@@ -33,32 +37,86 @@ def exit():
 	lcd.clear()
 	lcd.write_string('Bye Bye')
 
+# this function handles all button presses
+def button_handler(record,play,loop,enter):
+	# the current states of all buttons
+	record_state = 0
+	play_state = 0
+	loop_state = 0
+	enter_state = 0
+	# last known states
+	record_OLDstate = 0
+	play_OLDstate = 0
+	loop_OLDstate = 0
+	enter_OLDstate = 0
+
+	#initiate the GPIO pins
+	record_pin = 4
+	play_pin = 22
+	loop_pin = 23
+	enter_pin = 27
+	GPIO.setup(record_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(play_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(loop_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(enter_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+	# the event loop
+	while True:
+		record_state = GPIO.input(record_pin)
+		play_state = GPIO.input(play_pin)
+		loop_state = GPIO.input(loop_pin)
+		enter_state = GPIO.input(enter_pin)
+		if record_state != record_OLDstate:
+			#print("Button RECORD pressed!", record_state)
+			record.append(record_state)
+			record_OLDstate = record_state
+		if play_state != play_OLDstate:
+			#print("Button PLAY pressed!", play_state)
+			play.append(play_state)
+			play_OLDstate = play_state
+		if loop_state != loop_OLDstate:
+			#print("Button LOOP pressed!", loop_state)
+			loop.append(loop_state)
+			loop_OLDstate = loop_state
+		if enter_state != enter_OLDstate:
+			#print("Button ENTER pressed!", enter_state)
+			enter.append(enter_state)
+			enter_OLDstate = enter_state
+		time.sleep(0.1)
+
 def rotary_status(increment):
 	# init rotary encoder
 	clk = 17
 	dt  = 18
-	sw  = 27
 	inc = 0
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 	GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	clkLastState = GPIO.input(clk)
 	# the main daemon loop
 	while True:
 		clkState = GPIO.input(clk)
 		dtState = GPIO.input(dt)
-		switch = GPIO.input(sw)
-		if clkState != clkLastState:
-			print("rotary turned")
-			if dtState != clkState:
-				inc = 1
-			else:
-				inc = -1
+		# the following situation is for turning the knob anti-clockwise (left)
+		if clkState != clkLastState and clkState == dtState and clkState == 1:
+			inc = -1
+			#print("turning CCW")
+			#print("clk State:",clkState)
+			#print("dt  State:",dtState)
 			rot.append(inc)
+
+		# the following situation is for turning the knob clockwise (right)
+		if clkState != clkLastState and clkState != dtState and clkState == 1:
+			inc = 1
+			#print("turning CW")
+			#print("clk State:",clkState)
+			#print("dt  State:",dtState)
+			rot.append(inc)
+		# update the last state
 		clkLastState = clkState
-		time.sleep(0.01)
+		# sleep
+		time.sleep(0.003)
 
 def signal_handler(sig, frame):
 	exit()
@@ -69,7 +127,13 @@ def signal_handler(sig, frame):
 #rotary_queue = Queue(maxsize = 5)
 rot = deque([0,0,0,0,0],5)
 rotary = threading.Thread(target=rotary_status, args = (rot, ), daemon = True)
+record = deque([0],1)
+play = deque([0],1)
+loop = deque([0],1)
+enter = deque([0],1)
+buttons = threading.Thread(target=button_handler, args = (record,play,loop,enter, ), daemon = True)
 rotary.start()
+buttons.start()
 time.sleep(1)
 
 # main program loop
@@ -77,10 +141,7 @@ if __name__ == "__main__":
 	print("entering main part")
 	while True:
 		# check the rotary thread for new input
-		#print("\nFull: ", rotary_queue.full())
-		#print("\nEmpty: ", rotary_queue.empty())
-		#print("epmty deque")
-		while len(rot) > 1:
+		while len(rot) > 0:
 			item = rot.popleft()
 			counter += item
 			if counter > counter_max:
@@ -89,8 +150,38 @@ if __name__ == "__main__":
 				counter = counter_max
 			print(counter)
 		if counter != inc_old:
-			display_write(testset[counter],2,0)
+			display_write(str(counter),0,0)
+			display_write(testset[counter],2,0,clear=0)
 			inc_old = counter
+
+		# check the buttons thread for new input
+		while len(record) > 0:
+			item = record.popleft()
+			if item is 0:
+				print("Button RECORD pressed",item)
+				record_led.blink()
+			if item is 1:
+				print("Button RECORD released",item)
+				record_led.off()
+		while len(play) > 0:
+			item = play.popleft()
+			if item is 0:
+				print("Button PLAY pressed",item)
+			if item is 1:
+				print("Button PLAY released",item)
+		while len(loop) > 0:
+			item = loop.popleft()
+			if item is 0:
+				print("Button LOOP pressed",item)
+			if item is 1:
+				print("Button LOOP released",item)
+		while len(enter) > 0:
+			item = enter.popleft()
+			if item is 0:
+				print("Button ENTER pressed",item)
+			if item is 1:
+				print("Button ENTER released",item)
+
 
 		# check for exit condition
 		#signal.signal(signal.SIGINT, signal_handler)
